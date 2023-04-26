5ac92aff6c7d6527d7d1381d5a82a3787c4f6da0
bahfatma <bah06fatoumata@gmail.com>
2023-04-26 10:50:18 +0200

    ajout d'une nouvelle entreprise dans le fichier de config clents

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PlatsPageModule } from './platsPage/platsPage.module';
+import { PlatsPageController } from './platsPage/platsPage.controller';
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(
       require('../ormconfig.json')
     ),
+    PlatsPageModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, PlatsPageController],
   providers: [AppService],
 })
 export class AppModule {}

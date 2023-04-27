233c50073a2677e356eaa1ecd4dce4a240a7f58f
Amalhamdi007 <amal@24eme.fr>
2023-04-27 09:53:03 +0200

    fix header and footer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,6 @@
 import { Controller, Get, Render, Res } from '@nestjs/common';
-import { Response } from 'express';
-import { join } from 'path';
 
 @Controller()
 export class AppController {
-  @Get()
-  root(@Res() res: Response) {
-  res.sendFile(join(process.cwd(),'views', 'index.html'))
-  }
+  
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpAdapterHost } from '@nestjs/core';
+import { HomePageController } from './home-page/home-page.controller';
+
+
 
 @Module({
   //imports: [
@@ -9,8 +13,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       //require('../ormconfig.json')
     //),
   //],
-  imports: [],
-  controllers: [AppController],
+  imports: [ ],
+  controllers: [AppController, HomePageController],
   providers: [AppService],
 })
 export class AppModule {}

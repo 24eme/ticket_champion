e755dc24cf167ce256ef2bdab467b7eeaa184d37
bahfatma <bah06fatoumata@gmail.com>
2023-04-27 15:18:01 +0200

    affichage de la liste des plats à partir du fichier de config

--- src/plats.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PlatsController } from './plats.controller';
-
-@Module({
-    controllers: [PlatsController],
-  })
-  export class PlatsModule {}
--- src/platsPage/platsPage.controller.ts
+++ src/platsPage/platsPage.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { join } from 'path';
+import * as fs from 'fs';
 
-@Controller('plats')
+@Controller('/plats')
 export class PlatsPageController {
-  @Get()
-  index(): string {
-    return 'This is the plats page!';
+  @Get()  
+  @Render('plats')
+  async root() {
+    const data = await this.getPlatPage();
+    //creer un nouveau objet 
+    return { data : data}; 
   }
+  
+  async getPlatPage() {
+  const data = JSON.parse(fs.readFileSync('platsconfig.json', 'utf8'));
+  //console.log(data) 
+  return { data };
+  }
+
 }

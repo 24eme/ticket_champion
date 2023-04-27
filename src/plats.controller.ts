a64764e65bc72eed67da0f877a288168c6692454
bahfatma <bah06fatoumata@gmail.com>
2023-04-27 10:45:17 +0200

    Affichage de la liste des plats

---
+++ src/plats.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { join } from 'path';
+import * as fs from 'fs';
+
+@Controller('/plats')
+export class PlatsController {
+  @Get()  
+  @Render('plats')
+  async root() {
+    const data = await this.getPlatPage();
+    //creer un nouveau objet 
+    return { data : data}; 
+  }
+  
+  async getPlatPage() {
+  const data = JSON.parse(fs.readFileSync('platsconfig.json', 'utf8'));
+  //console.log(data) 
+  return { data };
+  }
+
+}

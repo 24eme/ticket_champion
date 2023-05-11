d31e6215c28709da08337140df2625c8d6064315
Amalhamdi007 <amal@24eme.fr>
2023-05-11 15:09:12 +0200

    fix function getClientCommandesGroupedByEntreprise and add form in restaurantPage

--- src/controller/restaurant.controller.ts
+++ src/controller/restaurant.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Post, Body, Req, Query } from '@nestjs/common';
 import { CommandeService } from 'src/services/commande.service';
 
-@Controller('restaurant')
+
+@Controller('/')
 export class RestaurantController {
 
     constructor(private readonly commandeService: CommandeService) {
@@ -9,11 +10,23 @@ export class RestaurantController {
       }
 
 
-    @Get('')
+    @Get('restaurant')
     @Render('restaurantPage')
     async restaurant() {
         const result = await this.commandeService.getClientCommandesGroupedByEntreprise();
         return { data: result };
     }
+  
+    @Post('/restaurant')
+    @Render('restaurantPage')
+
+  async postRestaurant(@Body() body: any, @Req() req : Request) {
+
+    console.log(req.body);
+    const keys = Object.keys(req.body);
+    console.log(keys[1]);
+   
+  }
+
 
 }
